9f120ec5d7f0aed4c049bdadd3e4f6b20d42747b
diyaryuce <diyargamingno@gmail.com>
2026-09-10 23:17:53 +0200

    2. Phone scaling, Under development so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return(<>
-    <div className=" flex justify-center items-center min-h-screen bg-[#151515]">
+    <div className=" flex justify-center items-center min-h-dvh bg-[#151515]">
       <h1 className="flex text-[clamp(1.425rem,4vw,2.5rem)] text-white font-bold shrink-0 ">
         drizzy.no is under development
       </h1>
