4faa5fa48cd8af691bea3787549aaaf1a565ab0f
diyaryuce <diyargamingno@gmail.com>
2026-09-10 23:27:45 +0200

    dev branch test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return(<>
     <div className=" flex justify-center items-center min-h-dvh bg-[#151515]">
       <h1 className="flex text-[clamp(1.425rem,4vw,2.5rem)] text-white font-bold shrink-0 ">
-        drizzy.no is under development
+        dev branch test
       </h1>
     </div>
   </>)
