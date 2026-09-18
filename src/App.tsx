0221ea43f153bdc910618f5ae21f9f3a1956acc9
diyaryuce <diyargamingno@gmail.com>
2026-09-18 15:08:16 +0200

    YuceCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 export default function App() {
-  return(<>
-    <div className=" flex justify-center items-center min-h-dvh bg-[#151515]">
-      <h1 className="flex text-[clamp(1.425rem,4vw,2.5rem)] text-white font-bold shrink-0 ">
-        drizzy.no is under development
-      </h1>
-    </div>
-  </>)
+  return (
+    <>
+      <div className=" flex justify-center items-center min-h-dvh bg-[#151515]">
+        <h1 className="flex text-[clamp(1.425rem,4vw,2.5rem)] text-white font-bold shrink-0 ">
+          yucecode.no is under development
+        </h1>
+      </div>
+    </>
+  );
 }
